7f66bacc460bb65d8cacded153102d1a3b1080f4
Andrew So <soandrew@users.noreply.github.com>
2017-12-04 13:49:15 -0500

    Code clean up

--- server.js
+++ server.js
@@ -767,12 +767,17 @@ app.route('/api/messages')
   })
   .post((req, res) => {
     if (req.user === 'admin') {
-      addMessage(req.body).then(elem => {
-        res.send(elem);
-      }).catch(err => {
-        console.log(err);
-        res.sendStatus(ERROR);
-      })
+      if (isValid(req.body, messageKeys)) {
+        addMessage(req.body).then(elem => {
+          res.send(elem);
+        }).catch(err => {
+          console.log(err);
+          res.sendStatus(ERROR);
+        })
+      } else {
+        res.status(INVALID_FORMAT);
+        res.send("Missing required properties: " + messageKeys);
+      }
     } else {
       res.sendStatus(UNAUTHORIZED);
     }
@@ -818,23 +823,12 @@ MongoClient.connect(dbURL, (err, res) => {
     collection = db.collection(COLLECTION_NAME);
     console.log("Connected to database using collection " + COLLECTION_NAME);
 
-    // TODO: Remove this
-    collection.drop();
-
     // Set up messages array
     collection.insertOne({
       user: "admin",
       password: "admin",
       messages: JSON.stringify([])
     }).catch(err => {
-      console.log(err);
-    })
-
-    // TODO: Remove this
-    addUser('john', 'a').catch(err => {  // Hard code user john
-      console.log(err);
-    });
-    addMessage({data: "hello"}).catch(err => {  // Hard code one message on server
       console.log(err);
     })
 
@@ -875,6 +869,7 @@ function addUser(user, password) {
 const vehicleKeys = ['name', 'id', 'description', 'active', 'first_flight', 'cost_per_launch', 'success_rate_pct'];
 const launchKeys = ['rocket.rocket_id', 'rocket.rocket_name', 'launch_site.site_id', 'launch_site.site_name_long', 'flight_number', 'details', 'rocket', 'launch_site', 'launch_date_local', 'launch_success'];
 const launchpadKeys = ['full_name', 'id', 'details', 'status', 'location'];
+const messageKeys = ['data'];
 
 // Helper function to validate input
 function isValid(elem, keys) {
@@ -1042,127 +1037,9 @@ function getLaunchpads(user, id) {
 }
 
 // ========== Add element to collection in database ==========
-
-/*
-Curl tests
-DELETE
-curl -u john:a --request DELETE localhost:3000/api/vehicles/falcon1
-curl -u john:a --request DELETE localhost:3000/api/launches/2
-curl -u john:a --request DELETE localhost:3000/api/launchpads/ccafs_slc_40
-
-PUT
-curl -u john:a -d '{"active": "true", "cost_per_launch" : "1"}' -H "Content-Type: application/json" --request PUT localhost:3000/api/vehicles/falcon9
-curl -u john:a -d '{"launch_success": "true"}' -H "Content-Type: application/json" --request PUT localhost:3000/api/launches/1
-curl -u john:a -d '{"full_name": "Deep Space Nine"}' -H "Content-Type: application/json" --request PUT localhost:3000/api/launchpads/ksc_lc_39a
-
-PUT (for create)
-curl -u john:a -d '{"name": "Falcon 10", "cost_per_launch": "100000000000", "success_rate_pct": "100", "first_flight": "2006-03-24",  "active": "true", "description":"The Falcon 10 is next gen"}' -H "Content-Type: application/json" --request PUT localhost:3000/api/vehicles/falcon10
-curl -u john:a -d '{"launch_date_local":"2006-03-25T10:30:00+12:00","rocket":{"rocket_id":"falcon1","rocket_name":"Falcon 1"},"launch_site":{"site_id":"kwajalein_atoll","site_name":"Kwajalein Atoll","site_name_long":"Kwajalein Atoll Omelek Island"},"launch_success":false,"details":"Engine failure at 33 seconds and loss of vehicle"}' -H "Content-Type: application/json" --request PUT localhost:3000/api/launches/100
-curl -u john:a -d '{"full_name":"Peace Moon","status":"active","location":{"name":"Cape Canaveral","region":"Florida"},"vehicles_launched":"falcon 9","details":"Fully operation battlestation"}' -H "Content-Type: application/json" --request PUT localhost:3000/api/launchpads/deathstar
-
-POST
-curl -u john:a -d '{"name": "Falcon 10", "cost_per_launch": "100000000000", "success_rate_pct": "100", "first_flight": "2006-03-24",  "active": "true", "description":"The Falcon 10 is next gen"}' -H "Content-Type: application/json" --request POST localhost:3000/api/vehicles/
-curl -u john:a -d '{"launch_date_local":"2006-03-25T10:30:00+12:00","rocket":{"rocket_id":"falcon1","rocket_name":"Falcon 1"},"launch_site":{"site_id":"kwajalein_atoll","site_name":"Kwajalein Atoll","site_name_long":"Kwajalein Atoll Omelek Island"},"launch_success":false,"details":"Engine failure at 33 seconds and loss of vehicle"}' -H "Content-Type: application/json" --request POST localhost:3000/api/launches/
-curl -u john:a -d '{"full_name":"Peace Moon","status":"active","location":{"name":"Cape Canaveral","region":"Florida"},"vehicles_launched":"falcon 9","details":"Fully operational battlestation"}' -H "Content-Type: application/json" --request POST localhost:3000/api/launchpads/
-*/
-
-//Enforces that the data does not already exist.
-function doesNotExist(data, id, attr) {
-  for (var i = 0; i < data.length; i++) {
-    console.log('Index %d', i, data[i][attr], id);
-    //Find JSON object with same id as what we're looking for
-    if(data[i][attr] === id){
-      console.log("Object with id %s already exists!", id.toString())
-      return 0; //Fail
-    }
-  }
-  return 1; //Nothing already exists, we can move on.
-}
-
-//Enforces that newdata has id, name, cost_per_launch, success_rate_pct, first_flight, active and description
-//We don't care if they have extra attributes as long as we satisfy these ones
-//Returns the id/key for this object as defined by attr on success and null on failure
-function addVehicleHelper(newdata, attr) {
-  var dictionary = {"id": false, "name": false, "cost_per_launch": false, "success_rate_pct": false, "first_flight": false, "active":false, "description":false};
-  for (var key in newdata) {
-    //Filter out hidden attributes of an object
-    if (newdata.hasOwnProperty(key)) {
-      console.log(newdata[key])
-      //Check if this is one of the ones we're looking for.
-      if (dictionary.hasOwnProperty(key)) {
-        dictionary[key] = true;
-      }
-    }
-  }
-  //Check if all conditions were satisfied
-  for (var key in dictionary) {
-    //Filter out hidden attributes of an object
-    if (dictionary.hasOwnProperty(key)) {
-      console.log(dictionary[key])
-      if (!dictionary[key]) {
-        return null; //Fails
-      }
-    }
-  }
-  return newdata[attr]; //Success
-}
-
-//'flight_number', 'details', 'rocket', 'launch_site', 'launch_date_local', 'launch_success'
-function addLaunchHelper(newdata, attr) {
-  var dictionary = {"flight_number": false, "details": false, "rocket": false, "launch_site": false, "launch_date_local": false, "launch_success":false};
-  for (var key in newdata) {
-    //Filter out hidden attributes of an object
-    if (newdata.hasOwnProperty(key)) {
-      console.log(newdata[key])
-      //Check if this is one of the ones we're looking for.
-      if (dictionary.hasOwnProperty(key)) {
-        dictionary[key] = true;
-      }
-    }
-  }
-  //Check if all conditions were satisfied
-  for (var key in dictionary) {
-    //Filter out hidden attributes of an object
-    if (dictionary.hasOwnProperty(key)) {
-      console.log(dictionary[key])
-      if (!dictionary[key]) {
-        return null; //Fails
-      }
-    }
-  }
-  return newdata[attr]; //Success
-}
-
-//'id', 'full_name', 'details', 'status', 'location'
-function addLaunchpadHelper(newdata, attr) {
-  var dictionary = {"id": false, "full_name": false, "details": false, "status": false, "location": false};
-  for (var key in newdata) {
-    //Filter out hidden attributes of an object
-    if (newdata.hasOwnProperty(key)) {
-      console.log(newdata[key])
-      //Check if this is one of the ones we're looking for.
-      if (dictionary.hasOwnProperty(key)) {
-        dictionary[key] = true;
-      }
-    }
-  }
-  //Check if all conditions were satisfied
-  for (var key in dictionary) {
-    //Filter out hidden attributes of an object
-    if (dictionary.hasOwnProperty(key)) {
-      console.log(dictionary[key])
-      if (!dictionary[key]) {
-        return null; //Fails
-      }
-    }
-  }
-  return newdata[attr]; //Success
-}
-
 function addVehicle(user, elem) {
   return getVehicles(user).then(data => {
     var id = elem.name.split(' ').join('').toLowerCase();
-    // TODO: check that this generated id isn't already used
     elem.id = id;
     data.push(elem);
     return collection.updateOne({user: user}, {$set: {vehicles: JSON.stringify(data)}}).then(res => {
@@ -1187,7 +1064,6 @@ function addLaunch(user, elem){
 function addLaunchpad(user, elem){
   return getLaunchpads(user).then(data => {
     var id = elem.full_name.split(' ').join('_').toLowerCase();
-    // TODO: check that this generated id isn't already used
     elem.id = id;
     data.push(elem);
     return collection.updateOne({user: user}, {$set: {launchpads: JSON.stringify(data)}}).then(res => {
@@ -1198,66 +1074,6 @@ function addLaunchpad(user, elem){
 }
 
 // ========== Update element in collection in database ==========
-
-//Returns the updated object with the id if updated else returns null
-//Mutate data inside helper before updating our collection
-//Last parameter is a function to enforce the attributes if we end up having to make a new entry
-function updateHelper(data, id, attr, newdata, enforceAttributes) {
-  //Tracks if we had any errors inside while trying to update key
-  var err = 0
-  var success = 0
-  for (var i = 0; i < data.length; i++) {
-    console.log('Index %d', i);
-    //Find JSON object with same id as what we're looking for
-    if(data[i][attr] == id){
-      console.log("Found our attribute!")
-      //For each attribute in -d '{"attr1": "foo"}', update our data with it.
-      for (var key in newdata) {
-        //Filter out hidden attributes of an object
-        if (newdata.hasOwnProperty(key)) {
-          console.log("Checking if data has key!")
-          //Make sure we aren't trying to update the superkey of the data
-          if (key == attr) {
-            err++;
-            break;
-          //Make sure that our original data already has this data as well, otherwise we are trying to update a non-existing field!
-          } else if (data[i].hasOwnProperty(key)) {
-            console.log(key + " -> " + newdata[key]);
-            console.log(key + " -> " + data[i][key]);
-            //Update the data!
-            data[i][key] = newdata[key]
-            success++;
-          } else {
-            //Can't update a non existing attribute!
-            err++;
-            break;
-          }
-        }
-      }
-      const message = "Success: " + success + " err: " + err;
-      // Print the string
-      console.log(message)
-      if (err == 0 && success > 0) { //Success! Return data that was updated
-        return data[i];
-      } else { //Error!
-        return null;
-      }
-    }
-  }
-  //Doesn't exist already and no errors -> Make new
-  if (err == 0) {
-    //Note that we don't allow the -d {} to contain attr otherwise someone can change the id of objects and we lose uniqueness
-    newdata[attr] = id; //Set it now.
-    //Enforce min attributes
-    if (enforceAttributes(newdata, attr)) {
-      data.push(newdata)
-      return data[data.length - 1];
-    }
-  }
-  //Didn't find anything
-  return null;
-}
-
 function updateVehicle(user, id, elem) {
   return getVehicles(user).then(data => {
     elem.id = id;
@@ -1310,26 +1126,6 @@ function updateLaunchpad(user, id, elem) {
 }
 
 // ========== Delete element to collection in database ==========
-
-//Returns a copy of the data on success
-//Takes in the JSON array, id to look for, and the attribute to check with.
-function deleteHelper(data, id, attr) {
-  //Just check for every index
-  for (var i = 0; i < data.length; i++) {
-    console.log('Index %d %s', i, JSON.stringify(data[i]));
-    //Look if the id is the same as our's
-    if(data[i][attr] == id){
-      //Make a copy!
-      var copy = JSON.parse(JSON.stringify(data[i])); //
-      //If we simply delete, we will have a null in the deleted entry's index. Thus, we must use splice to correct the array afterwards
-      delete data[i] //Seems to do the same thing even if we didn't delete first.
-      data.splice(i ,1);
-      return copy;
-    }
-  }
-  return null
-}
-
 function deleteVehicle(user, id) {
   return getVehicles(user).then(data => {
     var index = data.map(elem => elem.id).indexOf(id);
